76e4c9fe2bab459cffc7011d78ffaebbc7d7f9dd
Mohammed Elkady <melkady254@gmail.com>
2022-09-10 18:45:09 +0100

    Place class created

---
+++ src/Future/Collars/Place.java
@@ -0,0 +1,27 @@
+package Future.Collars;
+
+public class Place {
+    private int placeNumber;
+    private int rowNumber;
+    private boolean placeStatus;
+
+    public int getPlaceNumber() {
+        return placeNumber;
+    }
+    public int getRowNumber() {
+        return rowNumber;
+    }
+    public boolean getPlaceStatus() {
+        return placeStatus;
+    }
+
+    public void setPlaceNumber(int placeNumber) {
+        this.placeNumber = placeNumber;
+    }
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+    public void setPlaceStatus(boolean placeStatus) {
+        this.placeStatus = placeStatus;
+    }
+}
