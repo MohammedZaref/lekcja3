1650ae7c6354b3ec6af3b3e1a7095e1a5f37c404
Mohammed Elkady <melkady254@gmail.com>
2022-09-10 19:08:58 +0100

    Added a comment to show where's exercise starts and ends

--- src/Future/Collars/Main.java
+++ src/Future/Collars/Main.java
@@ -3,6 +3,7 @@ package Future.Collars;
 public class Main {
 
     public static void main(String[] args) {
+        // LEKCJA 3 EXERCISE 1 STARTS HERE
 	Place newPlaceInCinema = new Place();
 	newPlaceInCinema.setPlaceNumber(5);
 	newPlaceInCinema.setRowNumber(3);
@@ -11,5 +12,6 @@ public class Main {
         System.out.println("Place Number: " + newPlaceInCinema.getPlaceNumber());
         System.out.println("Row number: " + newPlaceInCinema.getRowNumber());
         System.out.println("Is busy: " + newPlaceInCinema.getPlaceStatus());
+        // LEKCJA 3 EXERCISE 1 ENDS HERE
     }
 }
