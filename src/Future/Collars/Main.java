86bb2a00c55ebb033cd8020b92b0e82ee5fdef8a
Mohammed Elkady <melkady254@gmail.com>
2022-09-10 18:53:24 +0100

    Setting the place number

---
+++ src/Future/Collars/Main.java
@@ -0,0 +1,9 @@
+package Future.Collars;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Place newPlaceInCinema = new Place();
+	newPlaceInCinema.setPlaceNumber(5);
+    }
+}
