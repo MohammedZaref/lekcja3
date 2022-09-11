02dfd5873d1794cefce70acaa54298cd63fc14d8
Mohammed Elkady <melkady254@gmail.com>
2022-09-11 22:01:45 +0100

    Created extrawage to add the difference between Manager and ticketSeller salary

--- src/Future/Collars/Manager.java
+++ src/Future/Collars/Manager.java
@@ -1,4 +1,17 @@
 package Future.Collars;
 
 public class Manager extends BaseEmployee {
+    final int extraWage = 2000;
+    private int bonus;
+
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+
 }
