6fe1622c83c68c53db5c03df530af510119a8094
Mohammed Elkady <melkady254@gmail.com>
2022-09-11 20:35:29 +0100

    creared name,lastName,salary,workingYear variables

--- src/Future/Collars/BaseEmployee.java
+++ src/Future/Collars/BaseEmployee.java
@@ -1,8 +1,34 @@
 package Future.Collars;
 
 public class BaseEmployee {
-    private int salary = 3000;
-    private int workingYears;
+    private String firstName;
+    private String lastName;
+    int workingYears;
+    final int salary = 3000;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getYearsOfEmployment() {
+        return workingYears;
+    }
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setYearsOfEmployment(int yearsOfEmployment) {
+        this.workingYears = yearsOfEmployment;
+    }
 
 }
