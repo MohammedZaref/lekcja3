4fa2afb17cae05838df56b730ed1f751db57aa75
Mohammed Elkady <melkady254@gmail.com>
2022-09-17 15:59:14 +0100

    Defining new object (newPlaceInCinema)

--- src/Future/Collars/Main.java
+++ src/Future/Collars/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // LEKCJA 3 EXERCISE 1 STARTS HERE
-        Place newPlaceInCinema = new Place();
-        newPlaceInCinema.setPlaceNumber(5);
-        newPlaceInCinema.setRowNumber(3);
-        newPlaceInCinema.setPlaceStatus(true);
+        Place newPlaceInCinema = new Place(5,3,true);
+//        newPlaceInCinema.setPlaceNumber(5);
+//        newPlaceInCinema.setRowNumber(3);
+//        newPlaceInCinema.setPlaceStatus(true);
 
         System.out.println("Place Number: " + newPlaceInCinema.getPlaceNumber());
         System.out.println("Row number: " + newPlaceInCinema.getRowNumber());
@@ -16,6 +16,7 @@ public class Main {
 
         // EXERCISE 2 STARTS HERE
         Film newFilm = new Film();
+
         newFilm.setFilmTitle("The Social Network");
         newFilm.setDirectorFullName("David Fincher");
         newFilm.setDuration(120);
