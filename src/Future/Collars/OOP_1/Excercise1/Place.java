9434701efd8bb14f477aeda3826573f029807f09
Mohammed Elkady <melkady254@gmail.com>
2022-09-18 17:41:47 +0100

    Moved Place.java - remapping package

---
+++ src/Future/Collars/OOP_1/Excercise1/Place.java
@@ -0,0 +1,33 @@
+package Future.Collars.OOP_1.Excercise1;
+
+public class Place {
+    private int placeNumber;
+    private int rowNumber;
+    private boolean placeStatus;
+
+    public Place(int placeNumber, int rowNumber, boolean placeStatus) {
+        this.placeNumber = placeNumber;
+        this.rowNumber = rowNumber;
+        this.placeStatus = placeStatus;
+    }
+
+    public int getPlaceNumber() {
+        return placeNumber;
+    }
+    public int getRowNumber() {
+        return rowNumber;
+    }
+    public boolean getPlaceStatus() {
+        return placeStatus;
+    }
+
+    public void setPlaceNumber(int placeNumber) {
+        this.placeNumber = placeNumber;
+    }
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+    public void setPlaceStatus(boolean placeStatus) {
+        this.placeStatus = placeStatus;
+    }
+}
---
+++ src/Future/Collars/OOP_1/Excercise2/Film.java
@@ -0,0 +1,25 @@
+package Future.Collars.OOP_1.Excercise2;
+
+public class Film {
+    private final String title = "The Social Network";
+    private final String directorFullName = "David Fincher";
+
+    private final int duration = 120;
+    private final int productionYear = 2010;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirectorFullName() {
+        return directorFullName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+}
